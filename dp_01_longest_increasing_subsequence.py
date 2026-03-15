080856b792afe6784d60c345e0c770e04ac79191
SURYA SAHIL - 787 <suryasahil878@gmail.com>
2026-03-15 12:00:47 +0530

    Create dp_01_longest_increasing_subsequence.py

---
+++ dp_01_longest_increasing_subsequence.py
@@ -0,0 +1,17 @@
+def lengthOfLIS(nums):
+    if not nums:
+        return 0
+
+    dp = [1] * len(nums)
+
+    for i in range(len(nums)):
+        for j in range(i):
+            if nums[j] < nums[i]:
+                dp[i] = max(dp[i], dp[j] + 1)
+
+    return max(dp)
+
+
+# Example
+nums = [10, 9, 2, 5, 3, 7, 101, 18]
+print(lengthOfLIS(nums))
