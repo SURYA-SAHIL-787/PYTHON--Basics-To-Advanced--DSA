8b8474b5c07e926a85855fdf045f4b3f843b93c1
SURYA SAHIL - 787 <suryasahil878@gmail.com>
2026-04-20 19:32:09 +0530

    Implement B-Tree data structure for file system

---
+++ filesystem_btree.py
@@ -0,0 +1,79 @@
+class BTreeNode:
+    def __init__(self, leaf=False):
+        self.leaf = leaf
+        self.keys = []
+        self.children = []
+
+class BTree:
+    def __init__(self, t):
+        self.root = BTreeNode(True)
+        self.t = t
+
+    def insert(self, k):
+        root = self.root
+        if len(root.keys) == (2 * self.t) - 1:
+            temp = BTreeNode()
+            self.root = temp
+            temp.children.append(root)
+            self.split_child(temp, 0)
+            self.insert_non_full(temp, k)
+        else:
+            self.insert_non_full(root, k)
+
+    def insert_non_full(self, node, k):
+        i = len(node.keys) - 1
+        if node.leaf:
+            node.keys.append("")
+            while i >= 0 and k < node.keys[i]:
+                node.keys[i + 1] = node.keys[i]
+                i -= 1
+            node.keys[i + 1] = k
+        else:
+            while i >= 0 and k < node.keys[i]:
+                i -= 1
+            i += 1
+            if len(node.children[i].keys) == (2 * self.t) - 1:
+                self.split_child(node, i)
+                if k > node.keys[i]:
+                    i += 1
+            self.insert_non_full(node.children[i], k)
+
+    def split_child(self, parent, i):
+        t = self.t
+        node = parent.children[i]
+        new_node = BTreeNode(node.leaf)
+
+        parent.children.insert(i + 1, new_node)
+        parent.keys.insert(i, node.keys[t - 1])
+
+        new_node.keys = node.keys[t:]
+        node.keys = node.keys[:t - 1]
+
+        if not node.leaf:
+            new_node.children = node.children[t:]
+            node.children = node.children[:t]
+
+    def search(self, node, key):
+        i = 0
+        while i < len(node.keys) and key > node.keys[i]:
+            i += 1
+
+        if i < len(node.keys) and key == node.keys[i]:
+            return True
+
+        if node.leaf:
+            return False
+
+        return self.search(node.children[i], key)
+
+files = ["notes.txt", "data.csv", "image.png", "project.docx", "music.mp3"]
+
+btree = BTree(3)
+for file in files:
+    btree.insert(file)
+
+target = "project.docx"
+if btree.search(btree.root, target):
+    print(target, "found in file system")
+else:
+    print(target, "not found")
